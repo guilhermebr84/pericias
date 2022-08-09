7087cc345b814d7691a442b205204c940c3e2246
nilton <nfescame@gmail.comq:qclear>
2022-08-09 16:07:48 -0300

    ajusted carousel

--- src/components/CarouselComponent/index.js
+++ src/components/CarouselComponent/index.js
@@ -1,45 +1,109 @@
-import React from "react";
+import React, { useState } from "react";
 import {
-  MDBCarousel,
-  MDBCarouselInner,
-  MDBCarouselItem,
-  MDBCarouselElement,
-  MDBCarouselCaption,
-} from "mdb-react-ui-kit";
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption,
+} from "reactstrap";
 import img_5 from "../../images/carousel_img/05.jpg";
 import img_6 from "../../images/carousel_img/06.jpg";
 import img_7 from "../../images/carousel_img/07.jpg";
 
-export default function App() {
+const items = [
+  {
+    altText: "Slide 1",
+    caption: "Slide 1",
+    key: 1,
+    src: img_5,
+  },
+  {
+    altText: "Slide 2",
+    caption: "Slide 2",
+    key: 2,
+    src: img_6,
+  },
+  {
+    altText: "Slide 3",
+    caption: "Slide 3",
+    key: 3,
+    src: img_7,
+  },
+];
+
+function Example(props) {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  };
+
+  const slides = items.map((item) => {
+    return (
+      <CarouselItem
+        className='custom-tag'
+        tag='div'
+        key={item.id}
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+      >
+        <img
+          src={item.src}
+          alt={item.altText}
+          className='img-fluid'
+          style={{ width: "100%", height: "80vh", opacity: 0.6 }}
+        />
+        <CarouselCaption
+          captionText={item.caption}
+          captionHeader={item.caption}
+        />
+      </CarouselItem>
+    );
+  });
+
   return (
     <div>
-      <MDBCarousel showControls showIndicators>
-        <MDBCarouselInner>
-          <MDBCarouselItem className='active'>
-            <MDBCarouselElement src={img_5} alt='img carousel' />
-            <MDBCarouselCaption>
-              <h5>First slide label</h5>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </MDBCarouselCaption>
-          </MDBCarouselItem>
-          <MDBCarouselItem>
-            <MDBCarouselElement src={img_6} alt='img carousel' />
-            <MDBCarouselCaption>
-              <h5>Second slide label</h5>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </MDBCarouselCaption>
-          </MDBCarouselItem>
-          <MDBCarouselItem>
-            <MDBCarouselElement src={img_7} alt='img carousel' />
-            <MDBCarouselCaption>
-              <h5>Third slide label</h5>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </MDBCarouselCaption>
-          </MDBCarouselItem>
-        </MDBCarouselInner>
-      </MDBCarousel>
+      <style>
+        {`.custom-tag {
+              max-width: 100%;
+              height: 500px;
+              background: black;
+            }`}
+      </style>
+      <Carousel activeIndex={activeIndex} next={next} previous={previous}>
+        <CarouselIndicators
+          items={items}
+          activeIndex={activeIndex}
+          onClickHandler={goToIndex}
+        />
+        {slides}
+        <CarouselControl
+          direction='prev'
+          directionText='Previous'
+          onClickHandler={previous}
+        />
+        <CarouselControl
+          direction='next'
+          directionText='Next'
+          onClickHandler={next}
+        />
+      </Carousel>
     </div>
   );
 }
+
+export default Example;
